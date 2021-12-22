a56471178b9d604ac8b360203b3fea8e667bbca2
Kostadin Samardzhiev <k.samardzhiev@gmail.com>
2021-12-22 14:02:14 +0200

    Add core functionality of the Xylophone

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        sender.alpha = 0.5
+        playSound(soundName: (sender.titleLabel?.text!)!)
+        print("Key \(String(describing: sender.titleLabel?.text)) got pressed!")
+        
+        let seconds = 0.2
+        DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
+            sender.alpha = 1
+        }
+    }
+    
+    func playSound(soundName: String) {
+        guard let url = Bundle.main.url(forResource: soundName, withExtension: "wav") else {
+            print("Url not found!")
+            return
+        }
 
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 }
 
